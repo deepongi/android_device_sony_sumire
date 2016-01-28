b284c164389e5ca4bf4aa5c4e92cf44bc692594d
CTXz <patrick_pedersen_02032001@yahoo.com>
2016-01-28 21:15:17 +0100

    Kernel: Use cm_kitakami_sumire_defconfig
    Change-Id: I6dc428b940ca21fd5d9f6b251ba760dc299a6345

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,8 +6,7 @@ TARGET_BOOTLOADER_BOARD_NAME := E6653
 
 #Kernel
 TARGET_KERNEL_SOURCE := kernel/sony/kitakami
-TARGET_KERNEL_CONFIG := msm8994_defconfig
-TARGET_KERNEL_VARIANT_CONFIG := kitakami_defconfig
+TARGET_KERNEL_CONFIG := cm_kitakami_sumire_defconfig
 
 #D2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/clearpad/wakeup_gesture"
