1794be3228186bb600e42ebe96ea2c3d497b5980
CTXz <patrick_pedersen_02032001@yahoo.com>
2016-01-30 00:25:02 +0100

    Sumire: Dexpreopt: Disable Dexpreopt for prebuild apks
    Change-Id: I7180ee71fa7d6a88797639f23bc14c3e326ee1b3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,9 @@ TARGET_BOOTLOADER_BOARD_NAME := E6653
 TARGET_KERNEL_SOURCE := kernel/sony/kitakami
 TARGET_KERNEL_CONFIG := cm_kitakami_sumire_defconfig
 
+#dex2oatd
+DONT_DEXPREOPT_PREBUILTS := true
+
 #D2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/clearpad/wakeup_gesture"
 TARGET_TAP_TO_WAKE_STRING := true
