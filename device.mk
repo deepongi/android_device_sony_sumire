086ffd0d9a683b82ce1dfe424d87ea1303350caa
Adam Farden <adam@farden.cz>
2016-02-28 21:04:11 +0100

    thermanager: better support for clusters, and thermal hotplugging
    control CPU freq and hotplugging via the msm_performance module
    
    Signed-off-by: Adam Farden <adam@farden.cz>
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -84,10 +84,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.sumire
 
-# Simple PowerHAL
-PRODUCT_PACKAGES += \
-    power.sumire
-
 PRODUCT_AAPT_CONFIG := large
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
