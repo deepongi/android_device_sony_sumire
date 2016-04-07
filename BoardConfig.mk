89642d414482f4db5f392423709b89b6200cb320
zachariasmaladroit <zacharias.maladroit@gmail.com>
2016-04-08 00:11:04 +0200

    add support for exfat, ntfs and notify the system that ext4 partitions
    are larger than 2gb (internal storage)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,3 +26,10 @@ WIFI_BUS := PCIE
 BOARD_KERNEL_CMDLINE += androidboot.hardware=sumire
 
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/clearpad/wakeup_gesture"
+
+# Use these flags if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+
+TARGET_KERNEL_HAVE_EXFAT := true
+TARGET_KERNEL_HAVE_NTFS := true
