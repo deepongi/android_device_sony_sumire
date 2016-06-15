40589a868374a4804486eb4f1578efc812547c6f
Zito <zzitoz@gmail.com>
2016-06-15 23:10:27 +0100

    Slim'ify
    Change-Id: I0a8b767a839154a40c4be10d78cfe42fba1b7c52

--- slim.mk
+++ slim.mk
@@ -9,10 +9,10 @@ $(call inherit-product, device/sony/sumire/aosp_e6653.mk)
 $(call inherit-product, device/sony/kitakami/platform2.mk)
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Fingerprint for sumire (from stock)
 #PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
@@ -20,5 +20,5 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 #PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.1.1 14.6.A.1.236 2031203603 release-keys"
 
 # Override Product Name for crDroid
-PRODUCT_NAME := cm_sumire
+PRODUCT_NAME := slim_sumire
 PRODUCT_MODEL := Xperia Z5
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,4 +14,4 @@
 # limitations under the License.
 #
 
-add_lunch_combo cm_sumire-userdebug
+add_lunch_combo slim_sumire-userdebug
